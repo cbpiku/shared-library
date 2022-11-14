d55916a38a1f1b3b637666db065a6d7d99bada12
chandan kumar Bera <chandankumar.bera@magna.com>
2022-11-14 09:46:45 +0000

    Add Build Test and Release Approved

--- vars/uatInput.groovy
+++ vars/uatInput.groovy
@@ -1,7 +1,7 @@
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("/var/jenkins_home/workspace/approved.txt")
+    def file = new File("/var/jenkins_home/workspace/build-approved.txt")
     
     if (file.exists()){
       return true;
@@ -10,6 +10,30 @@ public class uatInput {
       println "Approval file does not exist."
     }
     
+    return false;
+  }
+
+  def buildIsTestApproved() {
+    def file = new File("/var/jenkins_home/workspace/test-approved.txt")
+
+    if (file.exists()) {
+      return true;
+    }
+    else {
+      println "Approval file does not exist"
+    }
+    return false;
+  }
+
+  def buildIsReleaseApproved() {
+    def file = new File("/var/jenkins_home/workspace/release-approved.txt")
+
+    if (file.exists()) {
+      return true;
+    }
+    else {
+      println "Approval file does not exist"
+    }
     return false;
   }
 
