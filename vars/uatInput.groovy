30cf8464dbe660b7beeb9495768ebe9551736f18
chandan kumar Bera <chandankumar.bera@magna.com>
2022-11-14 09:11:20 +0000

    add file in vars folder

---
+++ vars/uatInput.groovy
@@ -0,0 +1,16 @@
+public class uatInput {
+
+  def buildIsUatApproved() {
+    def file = new File("/var/jenkins_home/workspace/approved.txt")
+    
+    if (file.exists()){
+      return true;
+    }
+    else {
+      println "Approval file does not exist."
+    }
+    
+    return false;
+  }
+
+}
